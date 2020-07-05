c96e3196b0d59edde5c1dbf067486281d2bb6f8d
kolinkuang <kolinkuang@github.com>
2020-07-05 22:59:07 +0800

    First commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+
+// import {log} from './util.js'
+// log('xx')
+
+// import { createApp } from 'vue'
+// console.log(createApp)
